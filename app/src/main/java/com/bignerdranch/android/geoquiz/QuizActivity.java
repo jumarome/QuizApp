4b8d4aceafda2e198ea5db593312c65d9cdc9492
jumarome <jmanuelromero93@gmail.com>
2016-09-07 00:15:32 -0500

    Changing UI from English to Spanish

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -26,7 +26,9 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_mideast,false),
             new Question(R.string.question_africa,false),
             new Question(R.string.question_americas,true),
-            new Question(R.string.question_asia,true)
+            new Question(R.string.question_frontera,false),
+            new Question(R.string.question_censo,true)
+
     };
 
     private int mCurrentIndex = 0;
