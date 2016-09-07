bd56e93a85b0e7d17eb57bf7447176c7ff0691dc
jumarome <jmanuelromero93@gmail.com>
2016-09-06 23:50:50 -0500

    Removing comment code for previous button

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -44,6 +44,8 @@ public class CheatActivity extends AppCompatActivity {
 
     }
 
+
+
     private void setAnswerShownResult(boolean isAnswerShown) {
         Intent data = new Intent();
         data.putExtra(EXTRA_ANSWER_SHOWN,isAnswerShown);
