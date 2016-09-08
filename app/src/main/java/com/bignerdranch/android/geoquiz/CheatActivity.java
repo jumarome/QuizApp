aceabfbb8cc06df886431500ecb084ad4d6afc26
jumarome <jmanuelromero93@gmail.com>
2016-09-07 22:49:05 -0500

    Fix Users can rotate CheatActivity after they cheat to clear out the cheating result

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -13,12 +13,25 @@ public class CheatActivity extends AppCompatActivity {
     private TextView mAnswerTextView;
     private Button mShowAnswer;
     private boolean mAnswerIsTrue;
+    private boolean mCheater;
+
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
     private static final String EXTRA_ANSWER_SHOWN ="com.bignerdranch.android.geoquiz.answer_shown";
+    private static final String CHEATER="cheater";
+    private static final String ANSWER ="answer";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        if(savedInstanceState !=null){
+            mCheater = savedInstanceState.getBoolean(CHEATER,false);
+            mAnswerIsTrue = savedInstanceState.getBoolean(ANSWER,false);
+            setAnswerShownResult(mCheater);
+
+
+
+        }
         setContentView(R.layout.activity_cheat);
 
         //Retrieve the value from the Extra
@@ -26,6 +39,10 @@ public class CheatActivity extends AppCompatActivity {
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
         mShowAnswer = (Button) findViewById(R.id.show_answer_button);
+
+        //Keep the answer shown to the user if the screen rotates
+        if(mCheater)
+            mAnswerTextView.setText(mAnswerIsTrue? R.string.true_button:R.string.false_button);
 
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +54,7 @@ public class CheatActivity extends AppCompatActivity {
                 else{
                     mAnswerTextView.setText(R.string.false_button);
                 }
+                mCheater = true;
                 setAnswerShownResult(true);
             }
         });
@@ -44,7 +62,13 @@ public class CheatActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle savedState) {
+        super.onSaveInstanceState(savedState);
 
+        savedState.putBoolean(CHEATER,mCheater);
+        savedState.putBoolean(ANSWER,mAnswerIsTrue);
+    }
 
     private void setAnswerShownResult(boolean isAnswerShown) {
         Intent data = new Intent();
